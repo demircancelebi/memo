236e32bbb8970a48f1e436a71c6cc1509a2ce059
osmancankucuk <osmancankucuk061@gmail.com>
2021-03-10 18:58:44 +0300

    10.03

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
 <template>
-  <div id="nav">
-    <styles>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/categories">Categories</router-link> |
-      <router-link to="/questions">Questions</router-link>
-    </styles>
-  </div>
-  <router-view />
+  <div><Nav /></div>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/categories">Categories</router-link> |
+    <router-link to="/questions">Questions</router-link>
+  </div>  -->
+  <router-view id="app" class="text-center" />
+  <Footer />
 </template>
+<script>
+import Nav from './components/Nav.vue';
+import Footer from './components/Footer.vue';
 
-<style>
+export default {
+  name: 'App',
+  components: {
+    Nav,
+    Footer,
+  },
+};
+</script>
+<style scoped lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-2">
+      <div class="container-fluid">
+        <router-link to="/" class="navbar-brand">Home</router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <router-link to="/categories" class="nav-link pb-1"
+              >Categories</router-link
+            >
+            <li class="nav-item">
+              <router-link to="/questions" class="nav-link pb-1"
+                >Questions</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Nav',
+  props: {
+    msg: String,
+  },
+};
+</script>
+<style scoped lang="scss" >
+.nav-link:hover,
+.nav-link:focus,
+.nav-link:active {
+  border-bottom: 3px solid rgb(184, 255, 255);
+}
+</style>
