85fdedde422b6c4f9aba31a51e6ee84b95daa669
CydYrg <ceyda_yurga@hotmail.com>
2021-03-12 12:38:48 +0000

    add team page

---
+++ src/views/Team.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="team">
+    <!-- <div v-for="team in team" :key="team.value">
+      {{ team.text }}
+    </div>
+    <input type="text" v-model="TeamMembersName">
+    <button @click="selectTeamMember">Select Team Member</button> -->
+  </div>
+</template>
+
+<script>
+
+// export default {
+//   name: 'Team Member Name',
+//   created() {
+//     this.getDefaultTeamMemberName();
+//   },
+//   methods: {
+//     getDefaultTeamMemberName() {
+//       const defaultOptions = [
+//         {
+//           name: 'Armagan',
+//           value: 'Armagan',
+//         },
+//         {
+//           text: 'Atilla',
+//           value: 'Atilla',
+//         },
+//         {
+//           text: 'Osmancan',
+//           value: 'Osmancan',
+//         },
+//         {
+//           text: 'Mesut',
+//           value: 'Mesut',
+//         },
+//         {
+//             text:'Goktug',
+//             value:'Goktug',
+//         },
+//         {
+//             text:'Mert',
+//             value:'Mert',
+//         },
+//         {
+//             text:'Galip',
+//             value:'Galip',
+//         },
+//         {
+//             text:'Demircan',
+//             value:'Demircan',
+//         },
+//         {
+//             text:'Ceyda',
+//             value:'Ceyda'
+//         }
+//       ];
+
+//       if (localStorage.getItem('teamMemberName')) {
+//         this.teamMemberName = JSON.parse(localStorage.getItem('teamMemberName'));
+//       } else {
+//         this.teamMemberName = defaultOptions;
+//       }
+//     },
+//   },
+//   data() {
+//     return {
+//       _teamMemberName: '',
+//       get teamMemberName() {
+//         return this._teamMemberName;
+//       },
+//       set teamMemberName(value) {
+//         this._teamMemberName=value;
+//       },
+//       teamMember: [],
+//     };
+//   },
+// }
+</script>
