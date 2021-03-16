4ff6518211f7a4ef941a8b8f0855c75a44a8c00a
mesuthabibpasa <75528246+mesuthabibpasa@users.noreply.github.com>
2021-03-16 03:00:36 +0300

    adding q with cat

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,40 +1,88 @@
 <template>
   <div class="questions">
     <div v-for="category in categories" :key="category.value">
-      <button class="btn btn-secondary mb-1" @click="showQuestions(category)"
-      >{{ category.text }}</button>
+      <button class="btn btn-secondary mb-1" @click="setCategory(category)">
+        {{ category.text }}
+      </button>
     </div>
-        <div class="input-group justify-content-center" v-if="currentCategory">
-          <label for="questionInput">Question:
-            <input id="questionInput" type="text" v-model="tempQuestion"/>
-          </label>
-          <label for="questionInput">Answer:
-            <input id="answerInput" type="text" v-model="tempAnswer"/>
-          </label>
-          <button class="btn btn-success" @click="addQuestion">Add Question</button>
+    <div class="row input-group justify-content-center mt-3" v-if="curCategory">
+      <div class="col-4">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Question"
+          aria-label="inputQuestion"
+          v-model="tempQuestion"
+        />
+      </div>
+      <div class="col-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Answer"
+          aria-label="inputAnswer"
+          v-model="tempAnswer"
+        />
+      </div>
+      <div class="col-2 card">
+        <div v-for="(cat, key) in categories" :key="cat">
+          <input type="checkbox" :id="key" v-model="tempTags" :value="cat.value"/>
+          <label :for="key">{{ cat.value }}</label>
         </div>
+      </div>
+      <div class="row mt-2">
+        <div class="col">
+          <button class="btn btn-success" @click="addQuestion">
+            Add Question
+          </button>
+        </div>
+      </div>
+    </div>
     <h2>===</h2>
-    <div class="row justify-content-center container-fluid mb-4"
-    v-if="currentCategory && catHasQuestions">
-        <div class="card ml-2 col-4" v-for="quest in  questions[currentCategory]" :key="quest.q">
-            <div class="card-body">
-              <h5 class="card-title">{{quest.q}}</h5>
-              <p class="card-text">{{quest.a}}</p>
-              <a href="#" class="btn btn-danger align-bottom" @click="removeQuestion(quest)">X</a>
-            </div>
+    <div
+      class="row justify-content-center container-fluid mb-4"
+      v-if="curCategory && currentQuestions"
+    >
+      <div
+        class="card ml-2 col-4"
+        v-for="quest in currentQuestions"
+        :key="quest.q"
+      >
+        <div class="card-body">
+          <h5 class="card-title">{{ quest.q }}</h5>
+          <p class="card-text">{{ quest.a }}</p>
+          <a
+            href="#"
+            class="btn btn-danger align-bottom"
+            @click="removeQuestion(quest)"
+            >X</a
+          >
         </div>
+      </div>
     </div>
-    <h5 v-if="!currentCategory"> Please select a category to see questions!</h5>
-    <h5 v-else-if="!catHasQuestions" class="mt-3">No question is available for the category
-      <strong>{{currentCategory}}</strong>. Please add some!</h5>
-    <!-- {{ questions }}
-    this is questions -->
+    <h5 v-if="!curCategory">Please select a category to see relevant questions!</h5>
+    <h5 v-else-if="!currentQuestions.length" class="mt-3">
+      No question is available for the category
+      <strong>{{ curCategory }}</strong
+      >. Please add some!
+    </h5>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Questions',
+  data() {
+    return {
+      categories: [],
+      questions: [],
+      index: {},
+      curCategory: '',
+      tempQuestion: '',
+      tempAnswer: '',
+      tempTags: [],
+    };
+  },
   created() {
     if (localStorage.getItem('currentUser')) {
       try {
@@ -77,69 +125,65 @@ export default {
       }
     },
     getQuestions() {
-      const defaultQuestions = {};
-
-      this.categories.forEach((category) => {
-        defaultQuestions[category.value] = [];
-      });
+      const qs = [
+        {
+          q: 'ilk soru',
+          a: 'cevap',
+          tags: ['science', 'tech'],
+        },
+        {
+          q: '2. soru',
+          a: 'cevap',
+          tags: ['history', 'science', 'tech'],
+        },
+        {
+          q: '3. soru',
+          a: 'cevap',
+          tags: ['history', 'geography'],
+        },
+        {
+          q: '4. soru',
+          a: 'cevap',
+          tags: ['tech', 'geography'],
+        },
+      ];
 
       if (localStorage.getItem('questions')) {
         this.questions = JSON.parse(localStorage.getItem('questions'));
       } else {
-        this.questions = defaultQuestions;
+        this.questions = qs;
       }
-
-      Object.keys(this.questions).forEach((question) => {
-        this.index[question] = 0;
-      });
     },
-    showQuestions(category) {
-      // console.log(`Showing questions for ${category.value}`);
-      this.currentCategory = category.value;
+    setCategory(category) {
+      this.curCategory = category.value;
     },
     addQuestion() {
-      // questions = {
-      //   art: [
-      //     {
-      //       q: 'Soru cumlesi',
-      //       a: 'Cevap'
-      //     },
-      //     {
-      //       q: '2. Soru cumlesi',
-      //       a: '2. Cevap'
-      //     },
-      //   ]
-      // }
-      this.questions[this.currentCategory].push({
+      if (!this.tempQuestion || !this.tempAnswer || this.tempTags.length === 0) {
+        console.log('Please provide a valid question, answer and at least one tag!');
+        return;
+      }
+      this.questions.push({
         q: this.tempQuestion,
         a: this.tempAnswer,
+        tags: this.tempTags,
       });
 
       localStorage.setItem('questions', JSON.stringify(this.questions));
       this.tempQuestion = '';
       this.tempAnswer = '';
+      this.tempTags = [];
     },
     removeQuestion(quest) {
-      const qIndex = this.questions[this.currentCategory].indexOf(quest);
+      const qIndex = this.questions.indexOf(quest);
       if (qIndex >= 0) {
-        this.questions[this.currentCategory].splice(qIndex, 1);
+        this.questions.splice(qIndex, 1);
         localStorage.setItem('questions', JSON.stringify(this.questions));
       }
     },
   },
-  data() {
-    return {
-      categories: [],
-      questions: {},
-      index: {},
-      currentCategory: '',
-      tempQuestion: '',
-      tempAnswer: '',
-    };
-  },
   computed: {
-    catHasQuestions() {
-      return this.questions[this.currentCategory].length > 0;
+    currentQuestions() {
+      return this.questions.filter((question) => question.tags.includes(this.curCategory));
     },
   },
 };
