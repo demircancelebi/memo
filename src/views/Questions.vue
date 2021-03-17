a981359ab559727b4425bd7afcab64eb2f2d4f78
Demircan Celebi <demircancelebi@gmail.com>
2021-03-17 20:29:12 +0000

    Edit Questions page to add/remove questions with tags

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,34 +1,41 @@
 <template>
   <div class="questions">
-    <div v-for="category in categories" :key="category.value">
-      <button class="btn btn-secondary mb-1" @click="showQuestions(category)"
-      >{{ category.text }}</button>
+    <div class="container">
+      <table class="table">
+        <thead>
+          <th>Actions</th>
+          <th>Q</th>
+          <th>A</th>
+          <th>Tags</th>
+        </thead>
+        <tbody>
+          <tr v-for="q, qIndex in questions" :key="q.q">
+            <td><a href="#" @click.prevent="removeQuestion(qIndex)">Remove question</a></td>
+            <td>{{ q.q }}</td>
+            <td>{{ q.a }}</td>
+            <td>
+              <span v-for="t, tagIndex in q.tags" :key="t">
+                {{ t }}&nbsp;
+                <a href="#" @click.prevent="removeTag(q.tags, tagIndex)"
+                v-if="q.tags.length > 1">x</a>&nbsp;
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-        <div class="input-group justify-content-center" v-if="currentCategory">
-          <label for="questionInput">Question:
-            <input id="questionInput" type="text" v-model="tempQuestion"/>
-          </label>
-          <label for="questionInput">Answer:
-            <input id="answerInput" type="text" v-model="tempAnswer"/>
-          </label>
-          <button class="btn btn-success" @click="addQuestion">Add Question</button>
-        </div>
-    <h2>===</h2>
-    <div class="row justify-content-center container-fluid mb-4"
-    v-if="currentCategory && catHasQuestions">
-        <div class="card ml-2 col-4" v-for="quest in  questions[currentCategory]" :key="quest.q">
-            <div class="card-body">
-              <h5 class="card-title">{{quest.q}}</h5>
-              <p class="card-text">{{quest.a}}</p>
-              <a href="#" class="btn btn-danger align-bottom" @click="removeQuestion(quest)">X</a>
-            </div>
-        </div>
+    <div class="input-group justify-content-center">
+      <label for="questionInput">Question:
+        <input id="questionInput" type="text" v-model="tempQuestion"/>
+      </label>
+      <label for="questionInput">Answer:
+        <input id="answerInput" type="text" v-model="tempAnswer"/>
+      </label>
+      <label for="tagsInput">Tags:
+        <input id="tagsInput" type="text" v-model="tempTags"/>
+      </label>
+      <button class="btn btn-success" @click="addQuestion">Add Question</button>
     </div>
-    <h5 v-if="!currentCategory"> Please select a category to see questions!</h5>
-    <h5 v-else-if="!catHasQuestions" class="mt-3">No question is available for the category
-      <strong>{{currentCategory}}</strong>. Please add some!</h5>
-    <!-- {{ questions }}
-    this is questions -->
   </div>
 </template>
 
@@ -46,101 +53,48 @@ export default {
     if (!this.currentUser) {
       this.$router.back();
     }
-    this.getCategories();
     this.getQuestions();
   },
   methods: {
-    getCategories() {
-      const defaultCategories = [
-        {
-          text: 'Geography',
-          value: 'geography',
-        },
-        {
-          text: 'History',
-          value: 'history',
-        },
-        {
-          text: 'Science',
-          value: 'science',
-        },
-        {
-          text: 'Technology',
-          value: 'tech',
-        },
-      ];
-
-      if (localStorage.getItem('categories')) {
-        this.categories = JSON.parse(localStorage.getItem('categories'));
-      } else {
-        this.categories = defaultCategories;
+    removeQuestion(qIndex) {
+      this.questions.splice(qIndex, 1);
+      localStorage.setItem('questions', JSON.stringify(this.questions));
+    },
+    removeTag(tags, tagIndex) {
+      if (tags.length > 1) {
+        tags.splice(tagIndex, 1);
+        localStorage.setItem('questions', JSON.stringify(this.questions));
       }
     },
     getQuestions() {
-      const defaultQuestions = {};
-
-      this.categories.forEach((category) => {
-        defaultQuestions[category.value] = [];
-      });
-
       if (localStorage.getItem('questions')) {
         this.questions = JSON.parse(localStorage.getItem('questions'));
       } else {
-        this.questions = defaultQuestions;
+        this.questions = [];
       }
-
-      Object.keys(this.questions).forEach((question) => {
-        this.index[question] = 0;
-      });
-    },
-    showQuestions(category) {
-      // console.log(`Showing questions for ${category.value}`);
-      this.currentCategory = category.value;
     },
     addQuestion() {
-      // questions = {
-      //   art: [
-      //     {
-      //       q: 'Soru cumlesi',
-      //       a: 'Cevap'
-      //     },
-      //     {
-      //       q: '2. Soru cumlesi',
-      //       a: '2. Cevap'
-      //     },
-      //   ]
-      // }
-      this.questions[this.currentCategory].push({
+      const tags = this.tempTags.split(' ').map((item) => item.replace('#', ''));
+
+      this.questions.push({
         q: this.tempQuestion,
         a: this.tempAnswer,
+        tags,
       });
 
       localStorage.setItem('questions', JSON.stringify(this.questions));
       this.tempQuestion = '';
       this.tempAnswer = '';
-    },
-    removeQuestion(quest) {
-      const qIndex = this.questions[this.currentCategory].indexOf(quest);
-      if (qIndex >= 0) {
-        this.questions[this.currentCategory].splice(qIndex, 1);
-        localStorage.setItem('questions', JSON.stringify(this.questions));
-      }
+      this.tempTags = '';
     },
   },
   data() {
     return {
-      categories: [],
-      questions: {},
-      index: {},
-      currentCategory: '',
+      questions: [],
       tempQuestion: '',
       tempAnswer: '',
+      tempTags: '',
     };
-  },
-  computed: {
-    catHasQuestions() {
-      return this.questions[this.currentCategory].length > 0;
-    },
   },
 };
 </script>
